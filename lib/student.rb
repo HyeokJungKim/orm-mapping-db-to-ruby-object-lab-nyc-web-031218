d48ea9a3977325c85e39761fd8970e1a8825a873
Eric Kim <hyeokjungkim@gmail.com>
2018-03-20 13:27:48 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,29 +1,75 @@
+require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
-  def self.new_from_db(row)
+  def self.new_from_db(arr)
     # create a new Student object given a row from the database
+    student = Student.new()
+    student.id = arr[0]
+    student.name = arr[1]
+    student.grade = arr[2]
+    return student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    arr = DB[:conn].execute("SELECT * FROM students")
+    arr.map do |student|
+      Student.new_from_db(student)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students WHERE name = ?
+    SQL
+    arr = DB[:conn].execute(sql, name).flatten
+    Student.new_from_db(arr)
   end
-  
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade == ?
+    SQL
+    DB[:conn].execute(sql, grade)
+  end
+
+  def self.count_all_students_in_grade_9
+    Student.all_students_in_grade_X(9)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade != 12
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade == 10 LIMIT ?
+    SQL
+    DB[:conn].execute(sql, num)
+  end
+
+  def self.first_student_in_grade_10
+    arr = Student.first_X_students_in_grade_10(1).flatten
+    Student.new_from_db(arr)
+  end
+
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
