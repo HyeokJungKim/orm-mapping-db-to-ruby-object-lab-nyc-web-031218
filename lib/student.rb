93332cda26423ab5b10e6135a610e72a436c3267
Eric Kim <hyeokjungkim@gmail.com>
2018-03-20 14:22:43 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -23,10 +23,10 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
-    sqli = <<-SQL
+    sql = <<-SQL
     SELECT * FROM students WHERE name = ?
     SQL
-    arr = DB[:conn].execute(sqli, name).flatten
+    arr = DB[:conn].execute(sql, name).flatten
     Student.new_from_db(arr)
   end
 
